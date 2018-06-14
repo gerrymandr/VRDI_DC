8524e4c950108ca44d138d50ea6f823b064111c7
RKBuck1 <rbuck1@macalester.edu>
2018-06-14 11:43:00 -0400

    Added documentation (including docstrings) and cleaned up functions

--- discretecompactness/geo_discrete_measures.py
+++ discretecompactness/geo_discrete_measures.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jun 12 14:15:13 2018
 
-@author: assaf
+@author: assaf, ruth, katya, zach
 """
 import geopandas as gpd
 import pandas as pd
@@ -17,7 +17,7 @@ from zipfile import ZipFile
 import csv
 
 
-# data retrieval
+# Data retrieval
 def get_and_unzip(url, data_dir=os.getcwd()):
     basename = url.split("/")[-1]
     name_with_path = os.path.join(data_dir, basename)
@@ -31,16 +31,15 @@ def get_and_unzip(url, data_dir=os.getcwd()):
         zip_obj.extractall(data_dir)
         del(zip_obj)
 
-#State FIPS codes
-FIPS = ["01"]
+# State FIPS codes - specify here your state of interest
+FIPS = ["01"]       # Alabama
 
 
-
-#Create file path to VTD shapefile if it does not exist
+# Create file path to VTD shapefile if it does not exist
 if not os.path.exists(os.path.join(os.getcwd(), 'VTD_SHP')):
     os.makedirs(os.path.join(os.getcwd(),'VTD_SHP'))
 
-#Download VTD shapefiles for each FIPS code specified above
+# Download VTD shapefiles for each FIPS code specified above
 for FIP in FIPS:
     url = "https://www2.census.gov/geo/tiger/TIGER2012/VTD/tl_2012_"+FIP+"_vtd10.zip"
     get_and_unzip(url,"VTD_SHP")
@@ -51,18 +50,26 @@ for FIP in FIPS:
 if not os.path.exists(os.path.join(os.getcwd(), 'CD_SHP')):
     os.makedirs(os.path.join(os.getcwd(),'CD_SHP'))
     
-# Grab congressional district shapefile for US from census
+# Grab 113th congressional district TIGER shapefile from census
 url_cd = "https://www2.census.gov/geo/tiger/TIGERrd13/CD113/tl_rd13_us_cd113.zip"
 get_and_unzip(url_cd, "CD_SHP")
     
 
-# Create the data frame and query out one state
+# Create the data frames and query out state of interest
 df_vtds = gpd.read_file("VTD_SHP")
 df_cds = gpd.read_file("CD_SHP")
 df_statecds = df_cds.loc[df_cds['STATEFP'] == state_fip]
 
-    
+   
+# Functions for calculating discrete area and perimeter
+ 
 def get_discrete_area(df_container, df_units, area_col_name = "area_count"):
+    """Takes in two data frames, one of a larger container geometry (i.e., congressional
+    district) and one of a smaller unit geometry (i.e., VTDs). There is also the 
+    option to input a specified string for the output area column name. This function
+    calculates the number of smaller units within the larger geometries and appends these
+    in a new column to the larger geometries data frame."""
+    
     disc_area = np.zeros(len(df_container))
     for i in range(0,len(df_container)):
         count = 0
@@ -70,21 +77,28 @@ def get_discrete_area(df_container, df_units, area_col_name = "area_count"):
             if df_container.iloc[i].geometry.contains(df_units.iloc[j].geometry):
                 count += 1
         disc_area[i] = count
-        print(disc_area)
+        #print(disc_area)
     df_container[area_col_name] = disc_area
         
     
 def get_discrete_perim(df_container, df_units, perim_col_name = "perim_count"):
+    """Takes in two data frames, one of a larger container geometry (i.e., congressional
+    district) and one of a smaller unit geometry (i.e., VTDs). There is also the 
+    option to input a specified string for the output perimeter column name. This function
+    calculates the number of smaller units that intersect the boundaries of the larger 
+    geometries and appends these in a new column to the larger geometries data frame."""
+    
     disc_perim = np.zeros(len(df_container))
     for i in range(0,len(df_container)):
         count = 0
         for j in range(0,len(df_units)):
-            if df_units.iloc[j].geometry.touches(df_container.iloc[i].geometry):
+            if df_units.iloc[j].geometry.intersects(df_container.iloc[i].geometry.boundary):
                 count += 1
         disc_perim[i] = count
-        print(disc_perim)
+        #print(disc_perim)
     df_container[perim_col_name] = disc_perim
 
+# Function calls
 get_discrete_perim(df_statecds, df_vtds)
 get_discrete_area(df_statecds, df_vtds)
 
