a1f0bf6aaf43d80122ad0a0197f2aec0d54318eb
Katie Jolly <katiejolly6@gmail.com>
2018-06-15 14:14:13 -0400

    standardize district and state projection process

--- discretecompactness/continuous_perimeter_area_district.py
+++ discretecompactness/continuous_perimeter_area_district.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Jun  13 13:16:29 2018
 
-@author: eion
+@author: eion + katie
 """
 
 import os
@@ -13,29 +13,22 @@ import pandas as pd
 import pysal as ps
 import numpy as np
 from numpy import linalg as LA
+import math
 
 # visualization
 import matplotlib.pyplot as plt
 
-
-def find_district_utm(df):
-    utm_district = []
-    for ind in range(0, len(df)):
-        utm_longitude = [126, 120, 114, 108, 102, 96, 90, 84, 78, 72, 66]
-        i = abs(df_districts_key['intptlon'][ind]) # take the longitude for a particular district, adjusting for the fact that it's negative but the list is positive
-        utm_longitude.append(i) # append it to the list of utms
-        utm_rev = sorted(utm_longitude, reverse = True) # descending order
-#        print("i =", i)
-#        print("state =", df_districts_key["name"][ind])
-#        print("utm zone =", 9 + utm_rev.index(i))
-        utm_district.append(9 + utm_rev.index(i)) # append the index plus 9 (for centering) to the list of county utms
-#    print(utm_county)
-    print(len(df))
-    print(len(utm_district))
-    df['utm'] = str(utm_district) # figure out how to append a column
+def find_district_utm(df): # input is a dataframe composed of districts with coarse UTM zones
+    for i in range(0, len(df)): # iterate through districts in dataframe
+        district = df.iloc[[i]][['geoid','geometry','utm']] # isolate geometry and coarse UTM from district of interest
+        utm = str(math.floor((df.iloc[[i]].centroid.x+180)*59/354)+1).zfill(2) # compute UTM zone according to the district's centroid longitude
+        district_utm = district.assign(utm=utm) # update district's UTM with proper assignment
+        if i==0: # begin aggregating correct UTM list
+            district_utms = district_utm
+        else: # continue aggregating correct UTM list
+            district_utms = pd.concat([district_utms,district_utm])
+    df['utm'] = district_utms['utm'] # update dataframe with correct UTM zones
     return df
-
-
 
 # pull and prepare data
 data_folder = 'tl_rd13_us_cd113'
@@ -54,8 +47,6 @@ utms = ["02","04","05","10","11","12","13","14","15","16","17","18","19","20","5
 
 df_districts_key = pd.merge(df_districts, key, how = 'left', on = 'STATEFP') # connect Shapefile data to key
 df_districts_key.columns = map(str.lower, df_districts_key.columns) # make all column names miniscule
-df_districts_key['intptlon'] = df_districts_key.centroid.x # compute and append column for longitude of district centroid
-df_districts_key['intptlat'] = df_districts_key.centroid.y # compute and append column for latitude of district centroid
 df_districts_key = find_district_utm(df_districts_key) # call function to add column of UTM zones by district
 
 for utm in utms:
@@ -65,7 +56,7 @@ for utm in utms:
     df_key_area = df.assign(area_km2=df_area.values) # merge area values with GEOID info
     df_length = df.to_crs(epsg=utm_epsg).length/1000 # compute projected lengths (perimeters) in kilometers
     df_key_length = df.assign(length_km=df_length.values) # merge length values with GEOID info
-    if utm == utms[0]:
+    if utm == utms[0]: # begin aggregating area and perimeter lists
         areas = df_key_area
         lengths = df_key_length
     else:
@@ -75,4 +66,5 @@ for utm in utms:
 df_districts_key = pd.merge(df_districts_key, areas, how = 'left', on = ['statefp','cd113fp','geoid','namelsad','lsad','cdsessn','mtfcc','funcstat','aland','awater','intptlat','intptlon','name','utm']) # combine keyed "Shapefile" with area calculations
 df_districts_key = pd.merge(df_districts_key, lengths, how = 'left', on = ['statefp','cd113fp','geoid','namelsad','lsad','cdsessn','mtfcc','funcstat','aland','awater','intptlat','intptlon','name','utm']) # combine keyed "Shapefile" with length calculations
 
-df_districts_key.loc[df_districts_key['statefp'] == '42'][['statefp','cd113fp','area_km2','length_km']] # inspect a single state's district areas and perimeters
+#df_districts_key.loc[df_districts_key['statefp'] == '42'][['statefp','cd113fp','area_km2','length_km']] # inspect a single state's district areas and perimeters
+#df_districts_key.loc[df_districts_key['statefp'] == '42'][['statefp','cd113fp','intptlon','utm']] # inspect a single state's district centroid longitude and UTM assignment
